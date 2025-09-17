341900ac1d29b1f92df5d56d5626c2b401327bc0
Boris Tsarev <boris.tsarev@king.com>
2025-09-17 22:23:34 +0200

    Polynomial regression

---
+++ notebooks/learn/polynomial_regression.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.3
+# ---
+
+# %% [markdown]
+# # Polynomial Regression
+
+# %% [markdown]
+#
